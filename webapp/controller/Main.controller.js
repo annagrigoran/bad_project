73402dda7d69e9221043f6389b17b15d09911769
Anna Grigoran <ANNA.GRIGORAN@MINDSQUARE.DE>
2022-09-23 09:34:17 +0000

    master

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -9,7 +9,7 @@ sap.ui.define([
 
         return Controller.extend("testproject.controller.Main", {
             onInit: function () {
-
+                    //Test 3
             }
         });
     });
